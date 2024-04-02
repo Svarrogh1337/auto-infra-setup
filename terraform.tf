fdd9e3c72ce913dc3d2fd2aae4379ef0be273d4c
Hristo Hristov <me@hhristov.info>
2024-04-02 14:37:21 +0300

    Send state to s3

--- terraform.tf
+++ terraform.tf
@@ -5,8 +5,16 @@ terraform {
      version = "~> 5.43.0"
    }
  }
+ backend "s3" {
+   bucket = "tf-bknd"
+   key    = "terraform/state"
+   region = "eu-central-1"
+ }
 }
 provider "aws" {
   profile = var.aws_profile
   region  = var.aws_region
+}
+terraform {
+
 }
